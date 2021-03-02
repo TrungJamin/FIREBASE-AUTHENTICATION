a9e98fee971cfe053b02959691599f30f61ba577
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2021-03-02 20:40:07 +0700

    ALmost completed Authentication

--- app/JS/auth.js
+++ app/JS/auth.js
@@ -1,3 +1,26 @@
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    db.collection("data")
+      .get()
+      .then((snapshot) => {
+        setUp(snapshot.docs);
+      });
+    document.querySelector(".login-wrap").style.display = "none";
+    document.querySelector("#table").style.display = "block";
+    document.querySelector("#logout").style.display = "block";
+    // ...
+  } else {
+    // User is signed out
+    setUp([]);
+    document.querySelector(".login-wrap").style.display = "block";
+    document.querySelector("#table").style.display = "none";
+    document.querySelector("#logout").style.display = "none";
+  }
+});
+
 // SIGN UP
 
 const signup_btn = document.querySelector("#signup-btn");
@@ -9,10 +32,11 @@ signup_btn.addEventListener("click", (e) => {
   auth
     .createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
+      // Signed in
       const user = userCredential.user;
 
       // tai sao o day nen return ?
-      db.collection("users").doc(user.uid).set({
+      return db.collection("users").doc(user.uid).set({
         email: email,
       });
     })
@@ -20,9 +44,35 @@ signup_btn.addEventListener("click", (e) => {
       alert("Signed up successfully!");
       signup_email.value = "";
       signup_password.value = "";
-      location.assign("./../HTML/UserPage/UserInterface.html");
     })
     .catch((err) => {
       alert("Invalid Email");
     });
+});
+
+// SIGN IN
+const signin_btn = document.querySelector("#signin-btn");
+
+signin_btn.addEventListener("click", (e) => {
+  const email = document.querySelector(".login-form #signin-email").value;
+  const password = document.querySelector(".login-form #signin-password").value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+    })
+    .catch((error) => {
+      alert("Incorrect email or password!");
+    });
+});
+
+// LOG OUT
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  auth.signOut().then(() => {
+    console.log("Signed out");
+  });
 });
--- app/JS/index.js
+++ app/JS/index.js
@@ -3,3 +3,30 @@ const signup_password = document.querySelector(".login-form #signup-password");
 
 const signin_email = document.querySelector(".login-form #signin-email");
 const signin_password = document.querySelector(".login-form #signin-password");
+
+var table = document.querySelector("#table");
+function setUp(data) {
+  if (data.length > 0) {
+    let html = "";
+    data.forEach((doc) => {
+      html += `
+            <li><a>${doc.data().field} <span class="pull-right">${
+        doc.data().number
+      }</span></a></li>
+      `;
+    });
+    table.innerHTML = `
+        <div class="col-sm-4">
+            <!-- Category -->
+            <div class="single category">
+                <h3 class="side-title">Category</h3>
+                <ul class="list-unstyled">
+                    ${html}
+                </ul>
+            </div>
+        </div>
+    `;
+  } else {
+    table.innerHTML = "<h1> LOGIN TO VIEW DATA </h1>";
+  }
+}
