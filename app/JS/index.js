b9e7b1f2399e448d536c49f86a1dc81a9c59ae24
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2021-03-02 15:59:31 +0700

    Login html and css files

---
+++ app/JS/index.js
@@ -0,0 +1,5 @@
+const signup_email = document.querySelector(".login-form #signup-email");
+const signup_password = document.querySelector(".login-form #signup-password");
+
+const signin_email = document.querySelector(".login-form #signin-email");
+const signin_password = document.querySelector(".login-form #signin-password");
